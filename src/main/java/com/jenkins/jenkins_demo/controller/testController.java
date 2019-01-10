8f789f22ca87ffb35008c4d88cb67c269514edd1
duanwxmy <46565140+duanwxmy@users.noreply.github.com>
2019-01-11 00:51:21 +0800

    1

---
+++ src/main/java/com/jenkins/jenkins_demo/controller/testController.java
@@ -0,0 +1,13 @@
+package com.jenkins.jenkins_demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+	
+	@GetMapping("/test")
+	public String list() {
+		return "holle jenkins";
+	}
+}
